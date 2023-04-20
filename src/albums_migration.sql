f0763fae6deb8b5fb468d0a042ab22416c276ef0
Jaewoo Baek <jaewoo.baek01@gmail.com>
2023-04-20 11:15:53 -0500

    exercises working on

--- src/albums_migration.sql
+++ src/albums_migration.sql
@@ -4,6 +4,7 @@ CREATE TABLE albums (
                         id INT UNSIGNED NOT NULL AUTO_INCREMENT,
                         artist VARCHAR(50),
                         author_last_name  VARCHAR(100) NOT NULL,
-                        content TEXT NOT NULL,
+                        release_date INT NOT NULL,
+                        sales float,
                         PRIMARY KEY (id)
 );
